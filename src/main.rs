28abcd44e807b7403d465ca869d21fc4fa7cbb0e
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-09 16:15:06 +0000

    sdfg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[macro_use] extern crate rocket;
+use base64::{Engine as _, engine::general_purpose};
 use openssl::encrypt::{Encrypter, Decrypter};
 use openssl::rsa::{Rsa, Padding};
 use openssl::pkey::PKey;
@@ -10,8 +11,22 @@ async fn brow_entry() -> Result<NamedFile, std::io::Error> {
 }
 
 #[post("/encrypt", data="<private>")]
-async fn encrypt(private: Vec<u8>) -> Vec<u8> {
+// please put the `-> String` back in
+async fn encrypt(private: &str) {
+   let keypair = Rsa::generate(2048).unwrap();
+   let keypair = PKey::from_rsa(keypair).unwrap();
+   let data = private.as_bytes();
 
+   // Encrypt the data with RSA PKCS1
+   let mut encrypter = Encrypter::new(&keypair).unwrap();
+   encrypter.set_rsa_padding(Padding::PKCS1).unwrap();
+   // Create an output buffer
+   let buffer_len = encrypter.encrypt_len(data).unwrap();
+   let mut encrypted = vec![0; buffer_len];
+   // Encrypt and truncate the buffer
+   let encrypted_len = encrypter.encrypt(data, &mut encrypted).unwrap();
+   encrypted.truncate(encrypted_len);
+   println!("{:?}", encrypted);
 }
 
 #[get("/")]
