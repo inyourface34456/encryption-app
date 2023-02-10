271feb79bf23da78ba8a977ce1a14ace6d9fafdd
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-10 15:18:47 +0000

    asfzb

--- src/id_sys.rs
+++ src/id_sys.rs
@@ -1,6 +1,6 @@
+// this is all from the rocket example for a cheep pastebin clone.
 use std::borrow::Cow;
 use std::path::{Path, PathBuf};
-
 use rand::{self, Rng};
 use rocket::request::FromParam;
 
