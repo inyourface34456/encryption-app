bda953abdcc9bf609c59d1b6f27a48210053ca74
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-13 14:11:40 +0000

    blablabla

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use rocket::tokio::fs::File;
 use rocket::get;
 
 const DLIM: &str = "{delimanator}";
+const KEY_LENGTH: u32 = 1024;
 
 #[post("/encrypt", data="<private>")]
 async fn encrypt(private: &str) -> String {
@@ -16,33 +17,31 @@ async fn encrypt(private: &str) -> String {
     let passphrase = private[1];
     let data = private[0];
 
-    let rsa = Rsa::generate(1024).unwrap();
-    let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), passphrase.as_bytes()).unwrap();
+    let rsa = Rsa::generate(KEY_LENGTH).unwrap();
     let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
     
     let rsa = Rsa::public_key_from_pem(String::from_utf8_lossy(&public_key).as_bytes()).unwrap();
     let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
     let _ = rsa.public_encrypt(data.as_bytes(), &mut buf, Padding::PKCS1).unwrap();
 
-    general_purpose::URL_SAFE_NO_PAD.encode(String::from_utf8_lossy(&buf).to_string())
+    general_purpose::STANDARD.encode(String::from_utf8_lossy(&buf).to_string())
 }
 
 #[post("/decrypt", data="<data>")]
 async fn decrypt(data: String) -> String {
     let private: Vec<&str> = data.split(DLIM).collect();
     let passphrase = private[1];
-    let data = general_purpose::URL_SAFE_NO_PAD.decode(private[0]).unwrap();
+    let data = general_purpose::STANDARD.decode(private[0]).unwrap();
     println!("{}", String::from_utf8_lossy(&data));
 
-    let rsa = Rsa::generate(1024).unwrap();
+    let rsa = Rsa::generate(KEY_LENGTH).unwrap();
     let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), passphrase.as_bytes()).unwrap();
-    let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
     
     let rsa = Rsa::private_key_from_pem_passphrase(String::from_utf8_lossy(&private_key).as_bytes(), passphrase.as_bytes()).unwrap();
     let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
     let _ = rsa.private_decrypt(&data, &mut buf, Padding::PKCS1).unwrap();
 
-    general_purpose::URL_SAFE_NO_PAD.encode(String::from_utf8_lossy(&buf).to_string())
+    general_purpose::STANDARD.encode(String::from_utf8_lossy(&buf).to_string())
 }
 
 #[get("/<folder_name>")]
