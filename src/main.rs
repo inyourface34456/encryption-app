96985729cc55553bf82cf1db0fcc0d88212de319
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2023-02-13 13:35:43 +0000

    lalalalallalalala

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,6 @@ async fn encrypt(private: &str) -> String {
     let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), passphrase.as_bytes()).unwrap();
     let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
     
-    let mut file = std::fs::File::create(format!("/workspaces/codespaces-blank/encryption-app/src/keys/{}priv.key", passphrase)).expect("create failed");
-    file.write_all(String::from_utf8_lossy(&private_key).as_bytes()).expect("write failed");
-    let mut file = std::fs::File::create(format!("/workspaces/codespaces-blank/encryption-app/src/keys/{}pub.key", passphrase)).expect("create failed");
-    file.write_all(String::from_utf8_lossy(&public_key).as_bytes()).expect("write failed");
-
     let rsa = Rsa::public_key_from_pem(String::from_utf8_lossy(&public_key).as_bytes()).unwrap();
     let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
     let _ = rsa.public_encrypt(data.as_bytes(), &mut buf, Padding::PKCS1).unwrap();
@@ -34,18 +29,20 @@ async fn encrypt(private: &str) -> String {
 
 #[post("/decrypt", data="<data>")]
 async fn decrypt(data: String) -> String {
-    let data: Vec<&str> = data.split(DLIM).collect();
-    let passphrase = data[1];
-    let raw_data = data[0];
-    println!("{}", passphrase);
-    let private_key_pem = std::fs::read(format!("/workspaces/codespaces-blank/encryption-app/src/keys/{}priv.key", passphrase)).unwrap();
+    let private: Vec<&str> = data.split(DLIM).collect();
+    let passphrase = private[1];
+    let data = general_purpose::URL_SAFE_NO_PAD.decode(private[0]).unwrap();
+    println!("{}", String::from_utf8_lossy(&data));
 
-    let rsa = Rsa::private_key_from_pem_passphrase(String::from_utf8_lossy(&private_key_pem).as_bytes(), passphrase.as_bytes()).unwrap();
+    let rsa = Rsa::generate(1024).unwrap();
+    let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), passphrase.as_bytes()).unwrap();
+    let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
+    
+    let rsa = Rsa::private_key_from_pem_passphrase(String::from_utf8_lossy(&private_key).as_bytes(), passphrase.as_bytes()).unwrap();
     let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
-    //error hear 
-    let _ = rsa.private_decrypt(general_purpose::URL_SAFE_NO_PAD.decode(raw_data).unwrap().as_slice(), &mut buf, Padding::PKCS1).unwrap();
+    let _ = rsa.private_decrypt(&data, &mut buf, Padding::PKCS1).unwrap();
 
-    String::from_utf8(buf).unwrap()
+    general_purpose::URL_SAFE_NO_PAD.encode(String::from_utf8_lossy(&buf).to_string())
 }
 
 #[get("/<folder_name>")]
